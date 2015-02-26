2585a8bce7bed5a828dc4bdcb13a21d15e86efca
Costas Sarris <costassarris@gmail.com>
2015-02-26 15:34:55 +0000

    users can sign in

--- bookmark_manager/app/server.rb
+++ bookmark_manager/app/server.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'data_mapper'
+require 'rack-flash'
 
 env = ENV['RACK_ENV'] || 'development'
 
@@ -18,6 +19,7 @@ class BookmarkManager < Sinatra::Base
 
   enable :sessions
   set :session_secret, 'super secret'
+  use Rack::Flash
 
   helpers do
     def current_user
@@ -47,14 +49,38 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :new_user
   end
 
   post '/users' do
-    user = User.create(:email => params[:email],
-                :password => params[:password])
-    session[:user_id] = user.id
-    redirect to '/'
+    @user = User.new(:email => params[:email],
+                  :password => params[:password],
+                  :password_confirmation => params[:password_confirmation])
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to '/'
+    else
+      flash[:errors] = @user.errors.full_messages
+      erb :"new_user"
+    end
+  end
+
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
+  post '/sessions' do
+    email, password = params[:email], params[:password]
+    user = User.authenticate(email, password)
+    if user
+      session[:user_id] = user.id
+      redirect to '/'
+    else
+      flash[:errors] = ["The email or password is incorrect"]
+      erb :"sessions/new"
+    end
   end
 
   #start the server if ruby file executed directly
--- bookmark_manager/spec/features/user_management_spec.rb
+++ bookmark_manager/spec/features/user_management_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "User sings up" do
+feature "User signs up" do
 
   scenario "when being a new user visiting the site" do
     expect{ sign_up }.to change(User, :count).by 1
@@ -8,11 +8,25 @@ feature "User sings up" do
     expect(User.first.email).to eq "alice@example.com"
   end
 
-  def sign_up(email = "alice@example.com", password = "oranges!")
+  scenario "with a password that doesn't match" do
+    expect{ sign_up('a@a.com', 'pass', 'wrong') }.to change(User, :count).by 0
+    expect(current_path).to eq '/users'
+    expect(page).to have_content "Password does not match the confirmation"
+  end
+
+  scenario "with an email that is already registered" do
+    expect{ sign_up }.to change(User, :count).by 1
+    expect{ sign_up }.to change(User, :count).by 0
+    expect(page).to have_content "This email is already taken"
+  end
+
+  def sign_up(email = "alice@example.com",
+              password = "oranges!",
+              password_confirmation = "oranges!")
     visit '/users/new'
-    expect(page.status_code).to eq 200
     fill_in :email, :with => email
     fill_in :password, :with => password
+    fill_in :password_confirmation, :with => password_confirmation
     click_button "Sign up"
   end
 
